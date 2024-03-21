b0c9c93231bc463aabde67b454fae8bf0c22aafc
KzeroJun <rladudwns164@gmail.com>
2024-03-21 16:23:55 +0900

    refactor : 인증서비스 구현 리팩토링

--- blog-backend/src/main/java/blog/kzerojunblog/service/implement/AuthServiceImplement.java
+++ blog-backend/src/main/java/blog/kzerojunblog/service/implement/AuthServiceImplement.java
@@ -10,84 +10,82 @@ import blog.kzerojunblog.provider.JwtProvider;
 import blog.kzerojunblog.repository.UserRepository;
 import blog.kzerojunblog.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthServiceImplement implements AuthService {
 
 	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	private final PasswordEncoder passwordEncoder;
 	private final JwtProvider jwtProvider;
 
 
 	@Override
 	public ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto) {
 		try {
-			String email = dto.getEmail();
-			boolean existedEmail = userRepository.existsByEmail(email);
 
-			if (existedEmail) {
+			if (isEmailExist(dto.getEmail())) {
 				return SignUpResponseDto.duplicateEmail();
 			}
 
-			String nickname = dto.getNickname();
-			boolean existedNickname = userRepository.existsByNickname(nickname);
-			if (existedNickname) {
+			if (isNicknameExist(dto.getNickname())) {
 				return SignUpResponseDto.duplicateNickname();
 			}
 
-			String telNumber = dto.getTelNumber();
-			boolean existedTelNumber = userRepository.existsByTelNumber(telNumber);
-			if (existedTelNumber) {
+			if (isTelNumberExist(dto.getTelNumber())) {
 				return SignUpResponseDto.duplicateTelNumber();
 			}
 
-			String password = dto.getPassword();
-			String encodedPassword = passwordEncoder.encode(password);
+			String encodedPassword = passwordEncoder.encode(dto.getPassword());
 			dto.setPassword(encodedPassword);
 
 			UserEntity userEntity = new UserEntity(dto);
 			userRepository.save(userEntity);
 
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (Exception exception) {
+			log.error("Error during sign up :", exception);
 			return ResponseDto.databaseError();
 		}
 
 		return SignUpResponseDto.success();
 	}
 
+	private boolean isEmailExist(String email) {
+		return userRepository.existsByEmail(email);
+	}
+
+	private boolean isNicknameExist(String nickname) {
+		return userRepository.existsByNickname(nickname);
+	}
+
+	private boolean isTelNumberExist(String telNumber) {
+		return userRepository.existsByTelNumber(telNumber);
+	}
+
 	@Override
 	public ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto) {
-		String token = null;
-
 		try {
-			String email = dto.getEmail();
-			UserEntity userEntity = userRepository.findByEmail(email);
-
+			UserEntity userEntity = userRepository.findByEmail(dto.getEmail());
 			if (userEntity == null) {
 				return SignInResponseDto.signInFailed();
 			}
 
-			String password = dto.getPassword();
-			String encodedPassword = userEntity.getPassword();
-			boolean isMatched = passwordEncoder.matches(password, encodedPassword);
+			boolean isMatched = passwordEncoder.matches(dto.getPassword(), userEntity.getPassword());
 
 			if (!isMatched) {
 				return SignInResponseDto.signInFailed();
 			}
+			String token = jwtProvider.create(dto.getEmail());
+			return ResponseEntity.ok(SignInResponseDto.success(token));
 
-			token = jwtProvider.create(email);
-
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (Exception exception) {
+			log.error("Error during sign in: ", exception);
 			return ResponseDto.databaseError();
 		}
-
-		return SignInResponseDto.success(token);
 	}
 }
