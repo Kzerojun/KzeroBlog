10db92e62094e0c49368e2f5cfad398ca1d896b4
KzeroJun <rladudwns164@gmail.com>
2024-03-23 13:35:05 +0900

    test : AuthService 테스트

--- blog-backend/src/test/java/blog/kzerojunblog/service/implement/AuthServiceImplementTest.java
+++ blog-backend/src/test/java/blog/kzerojunblog/service/implement/AuthServiceImplementTest.java
@@ -1,11 +1,15 @@
 package blog.kzerojunblog.service.implement;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 import blog.kzerojunblog.dto.request.auth.SignInRequestDto;
+import blog.kzerojunblog.dto.request.auth.SignUpRequestDto;
+import blog.kzerojunblog.dto.response.ResponseDto;
 import blog.kzerojunblog.dto.response.auth.SignInResponseDto;
+import blog.kzerojunblog.dto.response.auth.SignUpResponseDto;
 import blog.kzerojunblog.entity.UserEntity;
 import blog.kzerojunblog.provider.JwtProvider;
 import blog.kzerojunblog.repository.UserRepository;
@@ -35,15 +39,88 @@ class AuthServiceImplementTest {
 	private AuthServiceImplement authServiceImplement;
 
 	private UserEntity validUser;
+	private SignUpRequestDto signUpRequestDto;
 	private String validEmail = "user@example.com";
 	private String validPassword = "password";
 	private String invalidEmail = "wrong@example.com";
 	private String invalidPassword = "wrongpassword";
 	private String validToken = "validToken";
+	private String validTelNumber = "123-4567";
+	private String nickname = "nickname";
+	private final String address = "address";
+
+	private final String DETAIL_ADDRESS = "detail";
 
 	@BeforeEach
 	public void setUp() {
-		validUser = new UserEntity(validEmail, validPassword, "nickname", "123-4567", "address", "detail", "image", true);
+		signUpRequestDto = new SignUpRequestDto();
+		signUpRequestDto.setEmail(validEmail);
+		signUpRequestDto.setPassword(validPassword);
+		signUpRequestDto.setTelNumber(validTelNumber);
+		signUpRequestDto.setNickname(nickname);
+		signUpRequestDto.setAgreedPersonal(true);
+		signUpRequestDto.setAddress(address);
+		signUpRequestDto.setAddressDetail(DETAIL_ADDRESS);
+
+		validUser = new UserEntity(signUpRequestDto);
+	}
+
+	@Test
+	public void signUp_Success() {
+
+		when(userRepository.existsByEmail(validEmail)).thenReturn(false);
+		when(userRepository.existsByNickname("nickname")).thenReturn(false);
+		when(userRepository.existsByTelNumber("123-4567")).thenReturn(false);
+
+		ResponseEntity<? super SignUpResponseDto> response = authServiceImplement.signUp(
+				signUpRequestDto);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+
+		Object body = response.getBody();
+		assertThat(body).isInstanceOf(SignUpResponseDto.class);
+	}
+
+	@Test
+	public void signUp_Fail_Duplicate_Email() {
+
+		when(userRepository.existsByEmail(validEmail)).thenReturn(true);
+
+		ResponseEntity<? super SignUpResponseDto> response = authServiceImplement.signUp(
+				signUpRequestDto);
+
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
+		Object body = response.getBody();
+		assertThat(body).isInstanceOf(ResponseDto.class);
+	}
+
+	@Test
+	public void signUp_Fail_Duplicate_Nickname() {
+
+		when(userRepository.existsByNickname("nickname")).thenReturn(true);
+
+		ResponseEntity<? super SignUpResponseDto> response = authServiceImplement.signUp(
+				signUpRequestDto);
+
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
+		Object body = response.getBody();
+		assertThat(body).isInstanceOf(ResponseDto.class);
+	}
+
+	@Test
+	public void signUp_Fail_Duplicate_TelNumber() {
+
+		when(userRepository.existsByTelNumber(validTelNumber)).thenReturn(true);
+
+		ResponseEntity<? super SignUpResponseDto> response = authServiceImplement.signUp(
+				signUpRequestDto);
+
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
+		Object body = response.getBody();
+		assertThat(body).isInstanceOf(ResponseDto.class);
 	}
 
 	@Test
@@ -56,7 +133,8 @@ class AuthServiceImplementTest {
 		requestDto.setEmail(validEmail);
 		requestDto.setPassword(validPassword);
 
-		ResponseEntity<? super SignInResponseDto> response = authServiceImplement.signIn(requestDto);
+		ResponseEntity<? super SignInResponseDto> response = authServiceImplement.signIn(
+				requestDto);
 
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		SignInResponseDto responseBody = (SignInResponseDto) response.getBody();
@@ -72,7 +150,8 @@ class AuthServiceImplementTest {
 		requestDto.setEmail(invalidEmail);
 		requestDto.setPassword(validPassword);
 
-		ResponseEntity<? super SignInResponseDto> response = authServiceImplement.signIn(requestDto);
+		ResponseEntity<? super SignInResponseDto> response = authServiceImplement.signIn(
+				requestDto);
 
 		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
 	}
@@ -86,7 +165,8 @@ class AuthServiceImplementTest {
 		requestDto.setEmail(validEmail);
 		requestDto.setPassword(invalidPassword);
 
-		ResponseEntity<? super SignInResponseDto> response = authServiceImplement.signIn(requestDto);
+		ResponseEntity<? super SignInResponseDto> response = authServiceImplement.signIn(
+				requestDto);
 
 		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
 	}
